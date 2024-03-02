b2d102d3d6f2febe02491dd2b118522807998b78
curly lemon peel <alexpadillarosas@gmail.com>
2024-03-02 19:20:27 +1100

    Adding unwind segue for RedVC

--- Storyboard/RedVC.swift
+++ Storyboard/RedVC.swift
@@ -17,4 +17,12 @@ class RedVC: UIViewController {
     @IBAction func doSomethingGoBackDidPress(_ sender: Any) {
         print("button clicked")
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.destination.isKind(of: YellowVC.self){
+            //declare a constant to manipulate and immediately get it as reference of the destintion (YellowVC)
+            let yellowVC : YellowVC = segue.destination as! YellowVC
+            yellowVC.fruit = "Apple"
+        }
+    }
 }
--- Storyboard/YellowVC.swift
+++ Storyboard/YellowVC.swift
@@ -10,6 +10,7 @@ import UIKit
 class YellowVC: UIViewController {
 
     var contact : Contact!
+    var fruit : String = "Grapes"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,15 +23,16 @@ class YellowVC: UIViewController {
      */
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-//        if segue.destination.isKind(of: MintVC.self){
-//            print("Yes")
-//            var mintVC : MintVC = segue.destination as! MintVC
-//        }
-        
-        if let mintVC = segue.destination as? MintVC {
+        if segue.destination.isKind(of: MintVC.self){
             print("Yes")
+            let mintVC : MintVC = segue.destination as! MintVC
             mintVC.contact = contact
         }
+//        Another way to do the above
+//        if let mintVC = segue.destination as? MintVC {
+//            print("Yes")
+//            mintVC.contact = contact
+//        }
         
         
     }
@@ -38,12 +40,22 @@ class YellowVC: UIViewController {
      This function needs to be implemented before defining an unwind segue
      */
     @IBAction func unwindToYellowVC( _ unwindSegue: UIStoryboardSegue){
-        if let orangeVC = unwindSegue.source as? OrangeVC {
+        
+        if unwindSegue.source.isKind(of: OrangeVC.self){
             print("We came back from Orange")
         }
-        if let redVC = unwindSegue.source as? RedVC {
+        if unwindSegue.source.isKind(of: RedVC.self){
             print("We came back from Red")
+            print("We received the fruit \(fruit)")
         }
+        
+//        Another way to do it
+//        if let orangeVC = unwindSegue.source as? OrangeVC {
+//            print("We came back from Orange")
+//        }
+//        if let redVC = unwindSegue.source as? RedVC {
+//            print("We came back from Red")
+//        }
     }
     
 }
